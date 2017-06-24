2b4fdd8e2988a838695c3a0def19c76d3561f615
Cameron Albert <cameron@cameronalbert.com>
2017-06-24 00:03:00 -0400

    Stubbing out server.

---
+++ Beast.Abstractions/IServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beast
+{
+    public interface IServer
+    {
+        void Start();
+        void Stop();
+
+        Task ProcessInput(IConnection connection, string input);
+    }
+}
